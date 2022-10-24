4103144ad739a5805301b74aed1b67e45f051e1c
FogweProsper <fogweprosper@gmail.com>
2022-10-24 14:35:02 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes the pointer and updates the value
+ * @n: int to check
+ * Owned By Prospiano
+ * Return: 0 is a success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This has to do with all there is to strings, arrays, and pointers
+Tasks included here are meant to build my skills in these domains
