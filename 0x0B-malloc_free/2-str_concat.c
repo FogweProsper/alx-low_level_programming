69bb88940483cbd477adac86b908c1a753a613a7
FogweProsper <fogweprosper@gmail.com>
2022-11-09 06:09:18 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -40,7 +40,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 	for (j = 0; j < len2; j++, i++)
-		reslut[i] = s2[j];
+		result[i] = s2[j];
 	result[len] = '\0';
 
 	return (result);
